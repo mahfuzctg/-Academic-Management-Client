c6270adfe8d6124d701a51f6e8cf2290c0ee1aa8
Md Ekramul Haque <138347323+ekramul28@users.noreply.github.com>
2025-06-03 09:30:40 -0600

    updated footer code

--- src/pages/Home/Footer.tsx
+++ src/pages/Home/Footer.tsx
@@ -24,7 +24,7 @@ const footerLinks = {
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-50 border-t">
+    <footer className=" border-t">
       <div className="container mx-auto px-4 py-12">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           {/* Brand Section */}
