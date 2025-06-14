8862c2d155c68f0a8fe9e4134c2254391512d2b2
Md Ekramul Haque <138347323+ekramul28@users.noreply.github.com>
2025-06-14 07:36:19 -0600

    added enrollmentCourseApi added

---
+++ src/redux/features/enrollmentCourse/enrollmentCourseApi.ts
@@ -0,0 +1,74 @@
+import { baseApi } from "@/redux/api/baseApi";
+import type { TEnrolledCourse } from "@/types/enrolledCourse";
+import type { TQueryParam, TResponseRedux } from "@/types/global";
+
+const enrollmentCourseApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllEnrolledCourses: builder.query({
+      query: (args) => {
+        const params = new URLSearchParams();
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            params.append(item.name, item.value as string);
+          });
+        }
+        return {
+          url: "/enrolled-courses",
+          method: "GET",
+          params: params,
+        };
+      },
+      providesTags: ["EnrolledCourse"],
+      transformResponse: (response: TResponseRedux<TEnrolledCourse[]>) => ({
+        data: response.data,
+        meta: response.meta,
+      }),
+    }),
+
+    getMyEnrolledCourses: builder.query({
+      query: (args) => {
+        const params = new URLSearchParams();
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            params.append(item.name, item.value as string);
+          });
+        }
+        return {
+          url: "/enrolled-courses/my-enrolled-courses",
+          method: "GET",
+          params: params,
+        };
+      },
+      providesTags: ["EnrolledCourse"],
+      transformResponse: (response: TResponseRedux<TEnrolledCourse[]>) => ({
+        data: response.data,
+        meta: response.meta,
+      }),
+    }),
+
+    createEnrolledCourse: builder.mutation({
+      query: (data) => ({
+        url: "/enrolled-courses/create-enrolled-course",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["EnrolledCourse"],
+    }),
+
+    updateEnrolledCourseMarks: builder.mutation({
+      query: (data) => ({
+        url: "/enrolled-courses/update-enrolled-course-marks",
+        method: "PATCH",
+        body: data,
+      }),
+      invalidatesTags: ["EnrolledCourse"],
+    }),
+  }),
+});
+
+export const {
+  useGetAllEnrolledCoursesQuery,
+  useGetMyEnrolledCoursesQuery,
+  useCreateEnrolledCourseMutation,
+  useUpdateEnrolledCourseMarksMutation,
+} = enrollmentCourseApi;
