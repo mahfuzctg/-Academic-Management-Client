769cfbaa74b7efab4b9292f3c5087c8ca221cbdd
Md Ekramul Haque <138347323+ekramul28@users.noreply.github.com>
2025-06-19 23:31:34 -0600

    added academick year redux api

---
+++ src/redux/features/academic/academicYearApi.ts
@@ -0,0 +1,83 @@
+import { baseApi } from "@/redux/api/baseApi";
+import type { TQueryParam, TResponseRedux } from "@/types/global";
+
+export type TAcademicYear = {
+  _id: string;
+  name: string;
+  startDate: string;
+  endDate: string;
+  isActive: boolean;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export const academicYearApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    createAcademicYear: build.mutation({
+      query: (data) => ({
+        url: "/academic-years/create-academic-year",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["AcademicYear"],
+    }),
+
+    getSingleAcademicYear: build.query({
+      query: (id) => ({
+        url: `/academic-years/${id}`,
+        method: "GET",
+      }),
+      providesTags: ["AcademicYear"],
+    }),
+
+    updateAcademicYear: build.mutation({
+      query: ({ id, data }) => ({
+        url: `/academic-years/${id}`,
+        method: "PATCH",
+        body: data,
+      }),
+      invalidatesTags: ["AcademicYear"],
+    }),
+
+    deleteAcademicYear: build.mutation({
+      query: (id) => ({
+        url: `/academic-years/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["AcademicYear"],
+    }),
+
+    getAllAcademicYears: build.query({
+      query: (args?: TQueryParam[]) => {
+        const params = new URLSearchParams();
+
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            params.append(item.name, item.value as string);
+          });
+        }
+
+        return {
+          url: "/academic-years",
+          method: "GET",
+          params,
+        };
+      },
+      transformResponse: (response: TResponseRedux<TAcademicYear[]>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+      providesTags: ["AcademicYear"],
+    }),
+  }),
+});
+
+export const {
+  useCreateAcademicYearMutation,
+  useGetSingleAcademicYearQuery,
+  useUpdateAcademicYearMutation,
+  useDeleteAcademicYearMutation,
+  useGetAllAcademicYearsQuery,
+} = academicYearApi;
